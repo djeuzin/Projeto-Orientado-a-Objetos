71b70ed643b856fa6c437f8588f8be701fdbd00f
Rafael Freire <rafaelfmg72@gmail.com>
2026-04-07 22:48:06 -0300

    First commit

---
+++ notification.py
@@ -0,0 +1,60 @@
+# Rafael Freire Machado Gonçalves
+# RA 163977
+from abc import ABCMeta, abstractmethod
+
+# Classe base de notificação
+class Notification(metaclass=ABCMeta):
+	@abstractmethod
+	def send(self, msg: str) -> None:
+		...
+
+class SMSNotification(Notification):
+	def send(self, msg: str) -> None:
+		print(f"Sending SMS: {msg}")
+
+class EmailNotification(Notification):
+	def send(self, msg: str) -> None:
+		print(f"Sending email: {msg}")
+
+class PushNotification(Notification):
+	def send(self, msg: str) -> None:
+		print(f"Sending push notification: {msg}")
+
+class NotificationFactory:
+	@staticmethod
+	def new_notification(type=None):
+		match type:
+			case "SMS":
+				return SMSNotification()
+			case "EMAIL":
+				return EmailNotification()
+			case "PUSH":
+				return PushNotification()
+			case _:
+				print(f"Invalid message type")
+
+# Singleton metaclass
+class Singleton:
+	_instance = None
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = object.__new__(cls, *args, **kwargs)
+		return cls._instance
+
+# Classe singleton da configuração do sistema
+class SystemConfig(Singleton):
+	def __init__(self):
+		self.app_name = "App name"
+		self.server = "server.com"
+		self.max_retries = 100
+
+	def getConfig(self):
+		return self
+
+if __name__ == "__main__":
+	factory = NotificationFactory()
+	email = factory.new_notification("EMAIL")
+	email.send("algo")
+
+	sms = factory.new_notification("SMS")
+	sms.send("mensagem de sms")
---
+++ test_patterns.py
@@ -0,0 +1,25 @@
+from notification import *
+
+factory = NotificationFactory()
+
+def test_sms_obj():
+	a = factory.new_notification("SMS")
+	assert(type(a) == SMSNotification)
+
+def test_email_obj():
+	a = factory.new_notification("EMAIL")
+	assert(type(a) == EmailNotification)
+
+def test_push_obj():
+	a = factory.new_notification("PUSH")
+	assert(type(a) == PushNotification)
+
+def test_singleton():
+	a = SystemConfig()
+	b = SystemConfig()
+	assert a == b
+
+def test_factory():
+	a = factory.new_notification("SMS")
+	b = factory.new_notification("SMS")
+	assert a != b
